1e9b9dc544bc8d198e55e91b59043b082c77343b
Walter Werther <walter.werther@gmx.de>
2011-08-22 11:32:00 +0200

    Added first template

--- templates/bg_1.php
+++
@@ -1,9 +0,0 @@
-<?php
-// vim: set ts=4 et nu ai syntax=php indentexpr= :vim
-?>
-<div class="poem" style="background:url(http://wwerther.de/wp-content/gallery/poetry/bg_2.jpg); width:800px; height:532px">
-
-<div class="poem_box poem_title" style="left:20px;top:10px;width:390px;height:50px; line-height:50px; color:#FF0000"><?= $title ?></div>
-
-<div class="poem_box" style="left:20px;top:40px;width:390px;height:430px;line-height:<?= $lineheight?>px">
-
